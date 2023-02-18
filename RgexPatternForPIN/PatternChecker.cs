5afb91c9a45a35e76916fa4bbc99ab59aa4526ad
gavvalaganeshbabu <ganeshgavvala@gmail.com>
2023-02-19 01:43:44 +0530

    Add project files.

---
+++ RgexPatternForPIN/PatternChecker.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RgexPatternForPIN
+{
+    internal class PatternChecker
+    {
+        public static string PinCodePattern = "^[0-9]{6}$";
+    }
+}
---
+++ RgexPatternForPIN/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+using System.Text.RegularExpressions;
+
+namespace RgexPatternForPIN
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To Regex Pattern Matching Application!");
+            Console.WriteLine("Enter PIN Code ");
+            string PINCode=Console.ReadLine();
+            Regex regexPINChecker=new Regex(PatternChecker.PinCodePattern);
+            bool Check=regexPINChecker.IsMatch(PINCode);
+            if (Check == true)
+                Console.WriteLine("Entered PIN Code is Valid ");
+            else
+            Console.WriteLine("Entered PIN Code Is Not Valid ");
+
+        }
+    }
+}
